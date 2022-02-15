a9ea2f35c663aae532656952e563fe3f22915762
Andrea <andreakarellendoumbeabomo@gmail.com>
2022-02-15 11:28:58 +0100

    test

--- mesquestions.php
+++ mesquestions.php
@@ -23,7 +23,7 @@
         <div class="card-body">
           <p class="card-text"><?=$reponse['Description']; ?></p>
           <div class="row">
-            <a href="#" class="col btn btn-primary btn-sm me-2">Acceder à la question de Mr Alma chouchou</a>
+            <a href="#" class="col btn btn-primary btn-sm me-2">Acceder à la question</a>
           </div>
         </div>
     </div>
